4e01e1dd2261d699c8420cac8655d73d3437b063
Mkartey <mikekarty7@gmail.com>
2022-12-12 15:54:35 -0800

    A project on C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if ((a != 'e') & (a != 'q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
