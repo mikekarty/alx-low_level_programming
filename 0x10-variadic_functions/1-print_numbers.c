925eb3c9a09ff897850d2a424a094789b913f07e
Mkartey <mikekarty7@gmail.com>
2023-01-22 20:17:19 -0800

    A project of C - Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,44 +1,28 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * print_numbers - prints all numbers with a separator
- * @separator: separe numbers
- * @n: int, number of undefined arguments
- * print_numbers - prints numbers.
- * @separator: string to be printed between numbers.
- * @n: number of integers passed to the function.
- *
- * Return: no return.
+ * print_numbers - prints numbers
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: 0 always
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list list;
-unsigned int i;
-	va_list valist;
 	unsigned int i;
+	va_list ls;
 
-va_start(list, n);
-	va_start(valist, n);
+	va_start(ls, n);
 
-for (i = 0; i < n; i++)
-{
-	printf("%i", va_arg(list, int));
-	if (i != n - 1 && separator != NULL)
-		printf("%s", separator);
-}
-va_end(list);
+	if (separator == NULL)
+		separator = "";
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
+		printf("%d", va_arg(ls, int));
+		if (i < n - 1)
 			printf("%s", separator);
 	}
-
-putchar('\n');
 	printf("\n");
-	va_end(valist);
+	va_end(ls);
 }
